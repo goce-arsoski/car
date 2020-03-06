af9ac3e2a019fa8dac4d70824c3b6786fdb33f31
goce-arsoski <goce_ars@yahoo.com>
2020-03-06 19:48:10 +0100

    added new methods

--- lib/car.rb
+++ lib/car.rb
@@ -2,9 +2,10 @@ require "car/version"
 
 module Car
   class MyCar
+    SPEED_OFFSET = 15
 
     attr_accessor :color, :car_speed
-    attr_reader :year
+    attr_reader :year, :defects
 
     def initialize(year, color, brand, model)
       @year = year
@@ -12,11 +13,27 @@ module Car
       @brand = brand
       @model = model
       @car_speed = 0
+      @defects = []
     end
 
-    def speed_up(speed)
+    def speed_up(speed = SPEED_OFFSET)
       @car_speed += speed
     end
 
+    def slow_down(speed = SPEED_OFFSET)
+      @car_speed -= speed
+    end
+    def spray_paint(arg)
+      @color = arg
+    end
+
+    def add_defect(defect)
+      @defects << defect
+    end
+
+    def service_info
+      aux = @defects.join(', ')
+      "Defects need to be repaired: #{aux}."
+    end
   end
 end
--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -1,21 +1,62 @@
 RSpec.describe Car::MyCar do
+  let(:bmw) { Car::MyCar.new(2007, "metalic silver", "BMW", "325i") }
+
   it "creates instance of MyCar class" do
-    bmw = Car::MyCar.new(2007, "metalic silver", "BMW", "325i")
     expect(bmw).to be_instance_of(Car::MyCar)
   end
 
   it "has starting speed defined to 0" do
-    bmw = Car::MyCar.new(2007, "metalic silver", "BMW", "325i")
     expect(bmw.car_speed).to eq(0)
   end
 
   it "is driving in city" do
-    bmw = Car::MyCar.new(2007, "metalic silver", "BMW", "325i")
-    expect(bmw.speed_up(40)).to be_between(1, 50).inclusive
+    bmw.speed_up(40)
+    expect(bmw.car_speed).to be_between(1, 50).inclusive
   end
 
   it "is driving on highway" do
-    bmw = Car::MyCar.new(2007, "metalic silver", "BMW", "325i")
-    expect(bmw.speed_up(120)).to be_between(50, 131).exclusive
+    bmw.speed_up(120)
+    expect(bmw.car_speed).to be_between(50, 131).exclusive
+  end
+
+  it "is speeding up" do
+    bmw.speed_up
+    expect(bmw.car_speed).to eq(Car::MyCar::SPEED_OFFSET)
+  end
+
+  it "is speed up by default" do
+    bmw.speed_up
+    expect(bmw.car_speed).to eq(Car::MyCar::SPEED_OFFSET)
+  end
+
+  it "is slowing down" do
+    bmw.slow_down(20)
+    expect(bmw.car_speed).to be_between(-50, 50).inclusive
+  end
+
+  it "is slowing down by default" do
+    bmw.slow_down
+    expect(bmw.car_speed).to eq(-Car::MyCar::SPEED_OFFSET)
+  end
+
+  it "is changing color to" do
+    bmw.spray_paint :blue
+    expect(bmw.color).to eq(:blue)
+  end
+
+  it "has no defects" do
+    expect(bmw.defects).to be_empty
+  end
+
+  it "adds defects" do
+    bmw.add_defect("water pump")
+    expect(bmw.defects).to include("water pump")
+  end
+
+  it "list of defects" do
+    bmw.add_defect("water pump")
+    bmw.add_defect("oil leak")
+    bmw.add_defect("oil level sensor")
+    expect(bmw.service_info).to include("Defects need to be repaired: water pump, oil leak, oil level sensor.")
   end
 end
